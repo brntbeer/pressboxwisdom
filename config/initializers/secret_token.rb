c45ecbb0e1a7f709986d84b15b44cae3f356768d
Adam Coffman <coffman.adam@gmail.com>
2010-10-20 20:06:55 -0500

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pressboxwisdom::Application.config.secret_token = 'aa32766134e6ebac66679b6c1e6c1e2efbd4b82113546f11dbd6a5a81ec4da3568be5ae22b6d7aa479db70a498658d6ec4096e93d9b09ea2e338a1630ed1394c'
