933501dfd5382793a787cc5f215136b4d3a413e8
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 04:59:08 -0500

    enforce unique usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < Omnisocial::User
   has_many :posts
   has_many :comments
 
+  validates_uniqueness_of :nickname, :allow_blank => true, :allow_nil => true
+
   def preferred_name
     if nickname
       nickname
