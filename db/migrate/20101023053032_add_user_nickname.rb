60f361fb856a68c4a97728be71ba1fb64828f689
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 02:56:58 -0500

    whoops

--- db/migrate/20101023053032_add_user_nickname.rb
+++ db/migrate/20101023053032_add_user_nickname.rb
@@ -1,6 +1,6 @@
 class AddUserNickname < ActiveRecord::Migration
   def self.up
-    add_column :users, :nickname, :string, :null => false
+    add_column :users, :nickname, :string, :null => true
   end
 
   def self.down
