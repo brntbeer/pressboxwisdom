f377dd6f5b9250edb0981abbe3e49423478f494c
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 03:59:48 -0500

    started click to edit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
         $.notifyBar({ cls: $('#flash').attr('class'), html: $('#flash').html() });
   }
 
-  if($('#nickname').length){
+if ($('#nickname').length){
     $('#nickname').click(function() {
-        $('#nickname_input').html($('#nickname').html());
+        $('#nickname_input').val($('#nickname').html());
         $('#nickname').hide();
-        $('#nicname_input').fadeIn().focus();
+        $('#nickname_input').fadeIn().select().focus();
     });
     $('#nickname_input').blur(function(){
         previous = $('#nickname').html();
-        $('#nickname').html($('#nickname_input').html());
+        $('#nickname').html($('#nickname_input').val());
         $('#nickname_input').hide();
         $('#nickname').fadeIn();
         if(previous == $('#nickname').html()){
@@ -21,11 +21,7 @@ $(document).ready(function() {
     $('#nickname_input').keypress(function(event){
       if (event.keyCode == 13) {
        event.preventDefault();
-       $('#nickname_input').blur()
-      }else if (event.keyCode == 27){
-       $('#nickname_input').html(previous);
-       $('#nickname_input').blur()
-      }
+       $('#nickname_input').blur();
     });
   }
 
