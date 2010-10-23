a2151b0532c2e4f2bba2d6150c0464f806fe7436
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 03:46:26 -0500

    please work crazy bouverdaffs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,31 @@ $(document).ready(function() {
   if($('#flash').length){
         $.notifyBar({ cls: $('#flash').attr('class'), html: $('#flash').html() });
   }
+
+  if($('#nickname').length){
+    $('#nickname').click(function() {
+        $('#nickname_input').html($('#nickname').html());
+        $('#nickname').hide();
+        $('#nicname_input').fadeIn().focus();
+    });
+    $('#nickname_input').blur(function(){
+        previous = $('#nickname').html();
+        $('#nickname').html($('#nickname_input').html());
+        $('#nickname_input').hide();
+        $('#nickname').fadeIn();
+        if(previous == $('#nickname').html()){
+          //ajax callback
+        }
+    });
+    $('#nickname_input').keypress(function(event){
+      if (event.keyCode == 13) {
+       event.preventDefault();
+       $('#nickname_input').blur()
+      }else if (event.keyCode == 27){
+       $('#nickname_input').html(previous);
+       $('#nickname_input').blur()
+      }
+    });
+  }
+
 });
