7e8577e0432987ef7ad70bf19ddd435e456007ec
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 13:43:28 -0500

    tag view

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,16 @@ class MainController < ApplicationController
     @posts = Post.find(:all,:order => "created_at DESC", :limit => 10)
   end
 
+  def tags
+    @tag = params[:tag] || ""
+    tag = Tag.where(:tag => @tag ).first
+    if tag
+      @posts = tag.posts
+    else
+      @posts = []
+    end
+  end
+
   def update_nickname
     current_user.nickname = params[:nickname] unless params[:nickname] == "" 
     if current_user.save && params[:nickname] != ""
