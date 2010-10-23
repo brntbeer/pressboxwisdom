e96ab592c8f888a8e58d7343b2d8b0ab31330072
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 13:02:10 -0500

    a little better

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,8 @@ class MainController < ApplicationController
   end
 
   def update_nickname
-    current_user.nickname = params[:nickname]
-    if current_user.save
+    current_user.nickname = params[:nickname] unless params[:nickname] == "" 
+    if current_user.save && params[:nickname] != ""
       render :text => '', :status => 200
     else
       render :text => '', :status => 403
