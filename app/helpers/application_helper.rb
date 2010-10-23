2a369115b4642d4705f77d7e047b64e08c8f9e45
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 02:16:04 -0500

    partial fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def facebook_button
-    content_tag(:a, image_tag ("omnisocial/signin_facebook.png") , :href => auth_request_path(:service => 'facebook'))
+    content_tag(:a, image_tag("omnisocial/signin_facebook.png") , :href => auth_request_path(:service => 'facebook'))
   end
 
   def twitter_button
-    content_tag(:a, image_tag ("omnisocial/signin_twitter.png") , :href => auth_request_path(:service => 'twitter'))
+    content_tag(:a, image_tag("omnisocial/signin_twitter.png") , :href => auth_request_path(:service => 'twitter'))
   end
 
 end
