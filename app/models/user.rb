e493438824d6b699c80262ebb03a81c07b65186f
Adam Coffman <coffman.adam@gmail.com>
2010-10-23 00:34:53 -0500

    added nickname to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < Omnisocial::User
   has_many :posts
   has_many :comments
+
+  def prefered_name
+    if nickname
+      nickname
+    elsif from_twitter?
+      login
+    else
+      name
+  end
 end
