e89b769cbfa85f1f63e3902dd9652b87b58f3853
Brent Beer <brent.beer@gmail.com>
2010-10-30 12:38:13 -0500

    im picky. fixed it back but kept spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
     _max = histogram.map{|k,v| histogram[k]}.max * 2
     histogram.keys.sort{|a,b| histogram[a] <=> histogram[b]}.reverse.each do |k|
       next if histogram[k] < 1
-      _size = (((histogram[k] / histogram.map{|key,val| histogram[key]}.sum.to_f) * 100) ).to_i
+      _size = (( (histogram[k] / histogram.map{|key,val| histogram[key]}.sum.to_f) * 100 + 5)).to_i 
       _heat = sprintf("%02x" % (255 - (_size * 25)))
       html << %{
         <div class="heatmap_element" style="color: ##{_heat}#{_heat}#{_heat}; font-size: #{_size}px; height: #{_max+_size}px;"><a href=\"/tag/#{k}\">#{k}</a></div>
